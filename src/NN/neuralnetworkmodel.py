463131b3bb415ab8ad88b470aec1c66c879857bb
Perez Duran, Julio Cesar <julio.cesar.perez.duran@campus.tu-berlin.de>
2021-11-21 15:35:33 -0400

    data cleaning

--- src/NN/neuralnetworkmodel.py
+++ src/NN/neuralnetworkmodel.py
@@ -8,5 +8,19 @@ import matplotlib.pyplot as plt
 #%matplotlib inline
 
 df = pd.read_csv('../../data/pse_data.csv')
-print(df.head())
+#print(df.head())
 #df.head()
+
+#df.drop(df.tail(3).index,inplace=True) # drop last 3 rows
+
+N = 3
+# Drop last N columns of dataframe
+for i in range(N):
+        del df[df.columns.values[-1]]
+
+#print(df.isnull().sum())
+#index = df.index
+#number_of_rows = len(index)
+#print(number_of_rows)
+
+print(df.info)
