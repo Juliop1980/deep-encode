51c254893c353f6090b68c12b5a5685e7c6e800a
Juliop1980 <14-10820@usb.ve>
2021-11-29 14:08:09 -0400

    correlation analysis for vmaf

--- src/NN/neuralnetworkmodel.py
+++ src/NN/neuralnetworkmodel.py
@@ -13,10 +13,10 @@ df = pd.read_csv('../../data/pse_data.csv')
 
 #df.drop(df.tail(3).index,inplace=True) # drop last 3 rows
 
-N = 3
-# Drop last N columns of dataframe
-for i in range(N):
-        del df[df.columns.values[-1]]
+#N = 3
+## Drop last N columns of dataframe
+#for i in range(N):
+#        del df[df.columns.values[-1]]
 
 #print(df.isnull().sum())
 #index = df.index
@@ -24,5 +24,10 @@ for i in range(N):
 #print(number_of_rows)
 
 #print(df.info())
-
-print(df.corr())
+#print(df)
+correlations = df.corr()
+correlations_with_vmaf = correlations.iloc[: , -4]
+correlations_with_vmaf = correlations_with_vmaf.abs()
+correlations_with_vmaf = correlations_with_vmaf.sort_values(ascending=False)
+correlations_with_vmaf.drop(correlations_with_vmaf.tail(9).index,inplace=True)
+print(correlations_with_vmaf)
