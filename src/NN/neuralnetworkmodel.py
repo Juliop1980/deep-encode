6e7a83bc95360f358f3ca162e2a782e1da26b22e
Juliop1980 <14-10820@usb.ve>
2021-11-29 15:45:36 -0400

    correlation analysis text fields as categorical

--- src/NN/neuralnetworkmodel.py
+++ src/NN/neuralnetworkmodel.py
@@ -24,10 +24,20 @@ df = pd.read_csv('../../data/pse_data.csv')
 #print(number_of_rows)
 
 #print(df.info())
-#print(df)
+#print(df["e_codec_profile"])
+df["e_codec_profile"]=df["e_codec_profile"].astype('category').cat.codes
+df["c_content_category"]=df["c_content_category"].astype('category').cat.codes
+df["s_scan_type"]=df["s_scan_type"].astype('category').cat.codes
+df["e_scan_type"]=df["e_scan_type"].astype('category').cat.codes
+df["e_pixel_fmt"]=df["e_pixel_fmt"].astype('category').cat.codes
+
+
 correlations = df.corr()
+#print(correlations)
 correlations_with_vmaf = correlations.iloc[: , -4]
+#print(correlations_with_vmaf)
 correlations_with_vmaf = correlations_with_vmaf.abs()
 correlations_with_vmaf = correlations_with_vmaf.sort_values(ascending=False)
+#print(correlations_with_vmaf)
 correlations_with_vmaf.drop(correlations_with_vmaf.tail(9).index,inplace=True)
 print(correlations_with_vmaf)
