62181c3afed428352e47226bed9fd096e0c8c6f2
Perez Duran, Julio Cesar <julio.cesar.perez.duran@campus.tu-berlin.de>
2022-01-14 16:00:47 -0400

    ordered and commented plotting video script

--- src/NN/plot_video_data.py
+++ src/NN/plot_video_data.py
@@ -1,3 +1,5 @@
+# Script to plot the data predicted and the convex hull
+
 import os
 import collections
 import matplotlib.pyplot as plt
@@ -9,6 +11,7 @@ from scipy.spatial import ConvexHull, convex_hull_plot_2d
 colors_for_resolutions= {234:"#e3d05d",360:"#eb5d80", 432:"#7f86e5",540:"#f1a062",720:"#424346",1080:"#7db6e9"}
 resolution_height_width_dict = {234:416,360:640, 432:768,540:960,720:1280,1080:1920}
 
+# creates directory in current path
 def create_directory(directory_name):
     current_directory = os.getcwd()
     final_directory = os.path.join(current_directory, directory_name)
@@ -16,6 +19,7 @@ def create_directory(directory_name):
        os.makedirs(final_directory)
     return final_directory
 
+# Creates a directory with the same name as the id of the video inside the directory with the name passed as argument
 def create_directory_for_video(directory_name):
     current_directory = os.getcwd()
     final_directory = os.path.join(current_directory,"plots" ,directory_name)
@@ -23,6 +27,7 @@ def create_directory_for_video(directory_name):
        os.makedirs(final_directory)
     return final_directory
 
+# Creates a dictionary with the key being the resolution and the list of other variables as the value for each respective key
 def data_dictionary_by_resolution(list_of_data_by_resolution):
     dictionary = {}
     for i in list_of_data_by_resolution:
@@ -32,7 +37,6 @@ def data_dictionary_by_resolution(list_of_data_by_resolution):
 
     for i in list_of_data_by_resolution:
         resolution = i[1]
-       # dictionary[resolution] = []
         dictionary[resolution].append(i)
 
 
@@ -40,20 +44,24 @@ def data_dictionary_by_resolution(list_of_data_by_resolution):
     #print(dictionary)
     return dictionary
 
+# sorts a dictionary by the key
 def order_dict_by_key(dict):
      return collections.OrderedDict(sorted(dict.items()))
 
+# returns a list from the values of a dictionary
 def get_list_from_values(dict):
     values = dict.values()
     values_list = list(values)
     return values_list
 
+# order list by resolutions
 def order_by_resolution(list_of_data_by_resolution):
     dict = data_dictionary_by_resolution(list_of_data_by_resolution)
     dictionary_by_resolution = order_dict_by_key(dict)
     final_ordered_list = get_list_from_values(dictionary_by_resolution)
     return final_ordered_list
 
+# get maximum bitrate from a list of lists
 def get_max_bitrate(data_list_video):
     max_bitrate = 0
     for i in data_list_video:
@@ -61,11 +69,13 @@ def get_max_bitrate(data_list_video):
             max_bitrate = i[0]
     return max_bitrate
 
+# initialize graph with the scale for x as the max bitrate and the scale for y as the maximum vmaf
 def start_graph(max_bitrate,max_vmaf,title):
     plt.xlim(0, max_bitrate)
     plt.ylim(0, max_vmaf)
     plt.title(title)
     return plt
+
 
 def start_graph_with_max_bitrate_for_video(data_for_video_in_lists):
     max_bitrate = get_max_bitrate(data_for_video_in_lists)
@@ -74,19 +84,23 @@ def start_graph_with_max_bitrate_for_video(data_for_video_in_lists):
     graph = start_graph(max_bitrate, 100,title)
     return graph
 
+# get the height of the resolution from a list of lists of data
 def get_resolution_height(lists_with_resolution):
     first_list_with_resolution = lists_with_resolution[0]
     resolution_height = first_list_with_resolution[1]
     return resolution_height
 
+# get color for a certain resolution depending on the resolution height passed as the argument
 def get_color_for_resolution(resolution_height):
     color_hexadecimal = colors_for_resolutions[resolution_height]
     return color_hexadecimal
 
+# get bitrate from a list
 def get_bitrate(datos):
     bitrate = datos[0]
     return bitrate
 
+# get the vmaf of a list
 def get_vmaf(datos):
     vmaf = datos[2]
     return vmaf
@@ -133,106 +147,52 @@ def separate_resolutions_in_lists(df,video_id):
 
 
 
-#prueba = [[72000,1080,33],[722,720,33],[722,540,33]]
-#prueba2 = [[72000,1080,33],[722,1080,33],[722,1080,33]]
-
-# line 1 points
-#x1 = [1,2,3]
-#y1 = [2,4,1]
-## plotting the line 1 points
-##plt.plot(x1, y1, label = "line 1")
- 
-## line 2 points
-#x2 = [1,2,3]
-#y2 = [4,1,3]
-## plotting the line 2 points
-##plt.plot(x2, y2, label = "line 2")
- 
-## naming the x axis
-##plt.xlabel('x - axis')
-## naming the y axis
-##plt.ylabel('y - axis')
-## giving a title to my graph
-##plt.title('Two lines on same graph!')
- 
-## show a legend on the plot
-##plt.legend()
- 
-##start_graph(7000,100).show()
-##dict = data_dictionary_by_resolution(prueba)
-##ord_dict = order_dict_by_key(dict)
-##print(ord_dict)
-##print(get_list_from_values(ord_dict))
-##print(data_dictionary_by_resolution(prueba))
-##create_directory("prueba")
-##print(order_by_resolution(prueba))
-##start_graph_with_max_bitrate_for_video(prueba).show()
-##print(get_resolution_height(prueba2))
-##print(get_color_for_resolution(432))
-##datos = [72000,1080,33]
-##print(get_vmaf(datos))
-##smooth_resolution_graph(100,plt).show()
-
-#x1 = [1,2,3,4,5]
-#y1 = [3,5,7,12,20]
-## plotting the line 1 points
-##plt.plot(x1, y1, label = "line 1")
-
-##xnew = np.linspace(min(x1), max(x1), 300) 
-##spl = make_interp_spline(x1, y1, k=3)  # type: BSpline
-##power_smooth = spl(xnew)
-##plt.plot(xnew, power_smooth)
-##plt.show()
-##plot_smooth_resolution_graph(x1,y1,1080,plt).show()
-##graph =start_graph(7000,100,"encodding_ladder")
-
-#graph  = plot_smooth_resolution_graph(x1,y1,1080,plt)
-#store_graph_directory(graph, 123)
 
 df = pd.read_csv('../../data/data_with_predictions_neural_network.csv',delimiter=";")
-#df = df.astype({"s_video_id": int})
+
 N = 1
-#Drop last N columns of dataframe
 for i in range(N):
         del df[df.columns.values[-1]]
-#print(df)
 
 s_video_ids = df["s_video_id"].tolist()
 s_video_ids = list(set(s_video_ids))
 s_video_ids.sort()
+
+# create directory to store plots
 create_directory("plots")
 
-#rng = np.random.default_rng()
-#points = rng.random((30, 2))   # 30 random points in 2-D
-#print(type(points[0]))
-#hull = ConvexHull(points)
-#plt.plot(points[:,0], points[:,1], 'o')
-#for simplex in hull.simplices:
-#    plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
-##plt.show()
+# for every video
 
 for i in s_video_ids:
     points = []
-   # points = np.asarray(points)
+    # create a directory with the name of the video in the directory of plots
     directory_path = create_directory_for_video(str(i))
+
+    # split the data in lists order by resolution
     datos_for_videos_in_lists = separate_resolutions_in_lists(df,i)
     datos_for_videos_in_lists = order_by_resolution(datos_for_videos_in_lists)
     datos_for_videos_in_lists = sum(datos_for_videos_in_lists, [])
-    #print(datos_for_videos_in_lists
+    
+
     graph = start_graph_with_max_bitrate_for_video(datos_for_videos_in_lists)
-    ## naming the x axis
+    # naming the x axis
     plt.xlabel('Bitrate')
-    ## naming the y axis
+    # naming the y axis
     plt.ylabel('Vmaf')
+
+    #separate lists by resolutions
     datos_for_videos_in_lists = separate_resolutions_in_lists(df,i)
     datos_for_videos_in_lists = order_by_resolution(datos_for_videos_in_lists)
-    #print(datos_for_videos_in_lists)
+    # for every list with a resolution in a certain video:
     for resolution_list in datos_for_videos_in_lists:
+        # get the height of the resolution
         resolution_height = get_resolution_height(resolution_list)
-        #print(resolution_height)
+        
         color = get_color_for_resolution(resolution_height)
         bitrate_list = []
         vmaf_list = []
+        
+        # for every list from lists with a certain resolution:
         for datos in resolution_list:
             bitrate=get_bitrate(datos)
             bitrate_list.append(bitrate)
@@ -266,6 +226,7 @@ for i in s_video_ids:
     #graph.show()
 
     graph.show()
+    # uncomment to store graph in the directory it belongs (video)
     #store_graph_directory(graph,i)
     graph.clf()
     #store_graph_directory(graph,i)
